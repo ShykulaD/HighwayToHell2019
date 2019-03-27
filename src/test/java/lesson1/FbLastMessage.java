80e1ab88ac3dec7ed53b93f8c78b56c667712e8b
Shykulad <de.shykula@gmail.com>
2019-03-27 17:58:28 +0200

    3

--- src/test/java/lesson1/FbLastMessage.java
+++ src/test/java/lesson1/FbLastMessage.java
@@ -4,18 +4,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FbLastMessage {
 
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.facebook.com/");
         driver.findElement(By.cssSelector("[name='email']")).clear();
         driver.findElement(By.cssSelector("[name='email']")).sendKeys("pj.pdffiller@gmail.com");
         driver.findElement(By.cssSelector("[name='pass']")).clear();
         driver.findElement(By.cssSelector("[name='pass']")).sendKeys("Te12345st");
-        driver.findElement(By.id("u_0_2")).click();
+        driver.findElement(By.id("u_0_2")).click(); // click Submit button
+        driver.findElement(By.name("mercurymessages")).click();
+        driver.findElement(By.className("clearfix")).click();
         Thread.sleep(5000);
         driver.quit();
     }
