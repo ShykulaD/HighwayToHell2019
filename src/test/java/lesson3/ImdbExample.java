7ee876cf0a50c13f904ab28f4c0f68107d7cb5b7
Shykulad <de.shykula@gmail.com>
2019-04-02 20:20:12 +0300

    1

--- src/test/java/lesson3/ImdbExample.java
+++ src/test/java/lesson3/ImdbExample.java
@@ -8,6 +8,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class ImdbExample {
 
     public class SeleniumPractice {
@@ -15,44 +17,25 @@ public class ImdbExample {
         public WebDriver driver;
 
         @BeforeTest
-        public void setUp(){
+        public void setUp() {
             driver = new ChromeDriver();
+            driver.get("https://www.imdb.com/chart/top");
         }
 
         @Test
-        public void myTest(){
-            driver.get("https://www.imdb.com/chart/top");
-            list<WebElement> titles = driver.findElement(By.XPath("//*[@class='titleColumn']//a"));
-            for(WebElement element : titles) {
+
+        public void myTest() {
+            List<WebElement> elements = driver.findElements(By.xpath("//*[@class='titleColumn']//a"));
+            for (WebElement element : elements) {
                 System.out.println(element.getText());
             }
-
-        /*    driver.get("https://www.imdb.com/chart/top");
-            list<WebElement> titles = driver.findElement(By.XPath("//*[@class='titleColumn']//a"));
-            for(WebElement element : titles) {
-                System.out.println(element.getAttribute("href"));
-            }*/
-
-          /*  driver.get("https://www.imdb.com/chart/top");
-            list<WebElement> title = driver.findElement(By.XPath("//*[@class='titleColumn']//a"));
-            for(WebElement element : title) {
-                System.out.println(element.getAttribute("href"));*/
-            }
-
-
-
         }
 
-
-
-
-
-
-
         @AfterTest
-        public void tearDown(){
+        public void tearDown() {
             driver.quit();
         }
     }
-
 }
+
+
