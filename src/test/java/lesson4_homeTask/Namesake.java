684639c02ef61b8135a5f1280dc8c9e20e0f40c3
Shykulad <de.shykula@gmail.com>
2019-04-10 13:59:08 +0300

    '1'

---
+++ src/test/java/lesson4_homeTask/Namesake.java
@@ -0,0 +1,13 @@
+package lesson4_homeTask;
+
+public class Namesake {
+    public static void main(String[] args) {
+        String person1 = "Denys";
+        String person2 = "Denys";
+
+        if (person1.equals(person2)) {
+            System.out.println("Oh, you are nameSake's");
+        } else
+            System.out.println("Two names are different");
+    }
+}
