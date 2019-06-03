36bce70c2244819cbcfd55483effe6cf24646668
Shykulad <de.shykula@gmail.com>
2019-06-03 23:27:48 +0300

    add universalBank test

--- src/test/java/lesson7_homeTask/CurrencyInBanksDouble.java
+++ src/test/java/lesson7_homeTask/CurrencyInBanksDouble.java
@@ -12,9 +12,16 @@ public class CurrencyInBanksDouble extends BaseWrapper {
     public double ukrSibUsdBuy;
     public double ukrSibUsdSell;
 
+    public double universalUsdBuy;
+    public double universalUsdSell;
+
     private By privatUsd = By.xpath("//*[@class='section']/div[2]");
+
     private By ukrSibBankUsdBuy = By.xpath("//*[@class='currency__table']/tbody/tr[1]/td[2]");
     private By ukrSibBankUsdSell = By.xpath("//*[@class='currency__table']/tbody/tr[1]/td[3]");
+
+    private By universalBankUsdBuy = By.xpath("//*[@class='rate table table-bordered light fl-left m-t-2']/tbody/tr[2]/td[2]");
+    private By universalBankUsdSell = By.xpath("//*[@class='rate table table-bordered light fl-left m-t-2']/tbody/tr[2]/td[3]");
 
 /*
     @Test
@@ -36,14 +43,24 @@ public class CurrencyInBanksDouble extends BaseWrapper {
     @Test
     public void ukrSibBank()  {
         driver.get("https://my.ukrsibbank.com/ru/personal/operations/currency_exchange/");
-        String parseukrSibBankUsdBuy = driver.findElement(ukrSibBankUsdBuy).getText();
-        ukrSibUsdBuy = Double.parseDouble(parseukrSibBankUsdBuy); // USD for buying in ukrSibBank bank
+        String parseUkrSibBankUsdBuy = driver.findElement(ukrSibBankUsdBuy).getText();
+        ukrSibUsdBuy = Double.parseDouble(parseUkrSibBankUsdBuy); // USD for buying in ukrSibBank bank
 
         String parseukrSibBankUsdSell = driver.findElement(ukrSibBankUsdSell).getText();
-        ukrSibUsdSell = Double.parseDouble(parseukrSibBankUsdSell); // USD for selling in ukrSibBank bank
+        ukrSibUsdSell = Double.parseDouble(parseUkrSibBankUsdSell); // USD for selling in ukrSibBank bank
 
     }
 
 */
+
+    @Test
+    public void universalBank() {
+        driver.get("https://www.universalbank.com.ua//");
+        String parseUniversalBankUsdBuy = driver.findElement(universalBankUsdBuy).getText();
+        universalUsdBuy = Double.parseDouble(parseUniversalBankUsdBuy); // USD for buying in universal bank
+
+        String parseUniversalBankUsdSell = driver.findElement(universalBankUsdSell).getText();
+        universalUsdSell = Double.parseDouble(parseUniversalBankUsdSell); // USD for buying in universal bank
+    }
 
 }
